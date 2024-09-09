d2a39f88f5a9d50cc55c9a15568cd2b2f81e358b
hitius <kutay.incirci@gmail.com>
2024-09-09 13:34:25 +0300

    Initial commit

---
+++ src/test/java/com/ing/brokercore/BrokerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.brokercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
